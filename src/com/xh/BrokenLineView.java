247b5caad49c46770e0d30db281e77f0c2c82aea
liuhuiiang <825378291@qq.com>
2018-02-11 11:15:43 +0800

    折线图
    折线图

--- src/com/xh/BrokenLineView.java
+++ src/com/xh/BrokenLineView.java
@@ -1,1 +1,46 @@
-a
+package com.xh;
+
+import android.content.Context;
+import android.graphics.Path;
+import android.util.AttributeSet;
+
+/**
+ * PathMeasure com.xh 2018 2018-2-11 上午10:20:20 instructions： author:liuhuiliang
+ * email:825378291@qq.com
+ **/
+
+public class BrokenLineView extends PathView {
+
+	public BrokenLineView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BrokenLineView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BrokenLineView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setPoint(float[] points) {
+		if (points == null)
+			return;
+		int len = points.length;
+		if (len % 2 == 1)
+			len -= 1;
+		if (len == 0)
+			return;
+		Path path = new Path();
+		for (int i = 0; i < len; i += 2) {
+			if (i == 0) {
+				path.moveTo(points[i], points[i + 1]);
+			} else
+				path.lineTo(points[i], points[i + 1]);
+		}
+		addPath(path);
+	}
+}
